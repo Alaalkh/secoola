a5145bd63d9e21f8510855e6e1a93ad89228adcc
Alaa Alkhatib <aloo2ae@gmail.com>
2023-08-10 15:06:36 +0200

    Secoola

--- secoola/lib/main.dart
+++ secoola/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:secoola/Views/Screens/Splashscreen/SplashScreen.dart';
+import 'package:secoola/theme/Color.dart';
 
 
 import 'appRoutes.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
             title: 'Secoola',
             home:  SplachScreen(),
             theme: ThemeData(
-
+              scaffoldBackgroundColor: white,
               useMaterial3: true,
             ),
           );
