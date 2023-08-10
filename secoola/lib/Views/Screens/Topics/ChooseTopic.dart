fa0f6067eba72ed772ee16c7c1f48e002aa671a6
Alaa Alkhatib <aloo2ae@gmail.com>
2023-08-10 11:16:20 +0200

    Secoola

--- secoola/lib/Views/Screens/Topics/ChooseTopic.dart
+++ secoola/lib/Views/Screens/Topics/ChooseTopic.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/get_navigation.dart';
-import 'package:secoola/Models/TopicContent.dart';
-import 'package:secoola/appRoutes.dart';
 import 'package:secoola/theme/Color.dart';
 
 import 'TitleWidget.dart';
