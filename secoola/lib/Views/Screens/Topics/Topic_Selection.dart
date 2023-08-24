2bc5a385be1c608895ad5faa5590634bb50920e8
Alaa Alkhatib <aloo2ae@gmail.com>
2023-08-24 11:48:04 +0200

    Secoola

--- secoola/lib/Views/Screens/Topics/Topic_Selection.dart
+++ secoola/lib/Views/Screens/Topics/Topic_Selection.dart
@@ -1,5 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:secoola/Controllers/Controller.dart';
+import 'package:secoola/Models/Category.dart';
 import 'package:secoola/Models/TopicContent.dart';
 import 'package:secoola/theme/Color.dart';
 
@@ -12,6 +16,7 @@ class TopicSelection extends StatefulWidget {
 
 class _TopicSelectionState extends State<TopicSelection> {
   Set<int> selectedIndices = Set<int>();
+  final ApiController apiController = Get.put(ApiController());
 
   void toggleSelection(int index) {
     setState(() {
@@ -25,21 +30,31 @@ class _TopicSelectionState extends State<TopicSelection> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GridView.builder(
+    return FutureBuilder<ApiResponse>(
+      future: ApiController.fetchCategory(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Error loading data'));
+        } else if (!snapshot.hasData || snapshot.data!.data.isEmpty) {
+          return const Center(child: Text('No data available'));
+        } else {
+          final topics = snapshot.data!.data;
+          return GridView.builder(
             physics: const NeverScrollableScrollPhysics(),
             shrinkWrap: true,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 3,
-              crossAxisSpacing: 33.0,
-              mainAxisSpacing: 44.0,
-              mainAxisExtent: 100,
+              crossAxisSpacing: 44.0,
+              mainAxisSpacing: 11.0,
+              mainAxisExtent: 103,
             ),
-            itemCount: topicContent.length,
+            itemCount: topics.length,
             itemBuilder: (_, i) {
               bool isSelected = selectedIndices.contains(i);
-
+              final topicJson = topics[i];
+              final topic = ProgrammingTopic.fromJson(topicJson);
               return Column(
                 children: [
                   GestureDetector(
@@ -48,42 +63,51 @@ class _TopicSelectionState extends State<TopicSelection> {
                         toggleSelection(i);
                       });
                     },
-                    child: Stack(children: [
-                      Container(
-                        margin: EdgeInsets.only(left: 22.w, right: 16.w),
-                        height: 70.h,
-                        width: 101.w,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                            22.r,
+                    child: Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 22.w, right: 16.w),
+                          height: 78.h,
+                          width: 155.w,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(22),
+                            color: white2,
+                            border: Border.all(
+                              color: isSelected ? teal : white2,
+                              width: 2,
+                            ),
                           ),
-                          color: white2,
-                          border: Border.all(
-                            color: isSelected ? teal : white2,
-                            width: 2,
-                          ),
-                        ),
-                        child: Image.asset(topicContent[i].image),
-                      ),
-                      if (isSelected)
-                        Positioned(
-                          top:0,
-                          right:0,
-                          child: Image.asset(
-                            "assets/checkmark.png",
-                            color: teal,
+                          child: SizedBox(
+                            width: 66, // Your desired width
+                            height: 55, // Your desired height
+                            child: CachedNetworkImage(
+                              imageUrl: topic.image,
+                               // Adjust the fit based on your needs
+                            ),
                           ),
                         ),
-                    ]),
+                        if (isSelected)
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: Image.asset(
+                              "assets/checkmark.png",
+                              color: teal,
+                            ),
+                          ),
+                      ],
+                    ),
                   ),
                   Container(
                     margin: EdgeInsets.only(left: 11.w),
-                    child: Text(topicContent[i].title),
+                    child: Text(topic.name),
                   )
                 ],
               );
-            }),
-      ],
+            },
+          );
+        }
+      },
     );
   }
 }
